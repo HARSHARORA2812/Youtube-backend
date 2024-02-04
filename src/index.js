b13ecc2502813ae980ee7fa686168cd8469f0f33
harsha <harsha@harshas-MacBook-Air.local>
2024-02-05 00:00:28 +0530

    database connection

---
+++ src/index.js
@@ -0,0 +1,34 @@
+// require('dotenv').config({path : './env'})
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js ";
+
+dotenv.config({
+  path: './path'
+})
+
+import express from 'express';
+
+
+const app = express();
+
+connectDB()
+
+
+
+/*
+;( async () => {
+  try {
+   await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+   app.on("error", (error) => {
+     console.log(error);
+   })
+
+    app.listen(process.env.PORT,() => {console.log(`app listening on ${process.env.PORT}`);})
+
+  } catch (error) {
+    console.log(error);
+  }
+})()
+
+*/
