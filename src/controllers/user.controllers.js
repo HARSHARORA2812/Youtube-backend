864117b55cdb0b187b8481eb72fa3ce7e951de57
harsha <harsha@harshas-MacBook-Air.local>
2024-02-10 19:38:40 +0530

    registered controllers for users

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,74 @@
+import { ApiError } from "../utils/apiError.js";
 import {asyncHandler} from "../utils/asyncHandler.js";
+import {User} from "../models/user.models.js"
+import {uploadOnCloudibary} from "../utils/cloudnery.js"
+import { ApiResponse } from "../utils/apiResponse.js";
 
 const registerUser = asyncHandler(async (req,res)=> {
-   res.status(200).json({
-     message : "My code is running"
-   })
+   
+  const {fullName , username , email, password} = req.body;
+
+  console.log("Email : ", email)
+
+
+  // if(fullName === ""){
+  //   throw new ApiError(400 , "Fullname required")
+  // }
+
+  // -- i have to check every single onle like that but i am using sum property of array to concore this but i can use above code also to make it work
+
+  if (
+    [fullName , username , email, password].some((feild) => feild?.trim() === "")
+    ) {
+      throw new ApiError(400 , "All feilds are required") 
+  }
+   
+  const existUser = User.findOne({
+     
+     $or : [{ username } , { email }]
+     
+  })
+
+  if(existUser){
+    throw new ApiError(409 , "User existed already")
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400 , "avatar file is required ")
+  }
+
+  const avatar = await uploadOnCloudibary(avatarLocalPath);
+  const coverImage  = await uploadOnCloudibary(coverImageLocalPath);
+
+  if (avatar) {
+    throw new ApiError(400 , "avatar file is required ")
+  }
+
+  const user = await User.create({
+    fullName ,
+    avatar : avatar.url,
+    coverImage : coverImage?.url || "",
+    email,
+    password,
+    username : username.toLowerCase()
+  })
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  )
+   
+   if (!createdUser) {
+      throw new ApiError(500 , "something went wrong while registration")
+   }
+
+   return res.status(201).json(
+    new ApiResponse(200,createdUser,"user registered succefully")
+   )
+   
 })
 
-export {registerUser}
+
+
+export {registerUser} 
