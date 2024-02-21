cb331682b21a4d0083001df1b8d89d81c83bb646
Harsh <harsha@harshas-MacBook-Air.local>
2024-02-22 02:48:16 +0530

    fixed some bugs

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -8,12 +8,6 @@ const registerUser = asyncHandler(async (req,res)=> {
    
   const {fullName , username , email, password} = req.body;
 
-  // if(fullName === ""){
-  //   throw new ApiError(400 , "Fullname required")
-  // }
-
-  // -- i have to check every single onle like that but i am using sum property of array to concore this but i can use above code also to make it work
-
   if (
     [fullName , username , email, password].some((feild) => feild?.trim() === "")
     ) {
@@ -43,16 +37,18 @@ const registerUser = asyncHandler(async (req,res)=> {
   if (!avatar) {
     throw new ApiError(400 , "avatar file is required ")
   }
+  
+   console.log(avatar.url);
 
   const user = await User.create({
     fullName ,
-    avatar : avatar.links,
-    coverImage : coverImage?.links || "",
+    avatar : avatar.url,
+    coverImage : coverImage?.url || "",
     email,
     password,
     username : username.toLowerCase()
   })
-  
+
   const createdUser = await User.findById(user._id).select(
     "-password -refreshToken"
   )
--- src/utils/cloudnery.js
+++ src/utils/cloudnery.js
@@ -1,5 +1,4 @@
 import {v2 as cloudinary} from "cloudinary";
-import { response } from "express";
 import fs from "fs" 
           
 cloudinary.config({ 
@@ -14,11 +13,11 @@ const uploadOnCloudibary = async (localFilePath) => {
 
     if(!localFilePath) return null
 
-    cloudinary.uploader.upload(localFilePath,{
+    const response =await cloudinary.uploader.upload(localFilePath,{
       resource_type : "auto"
     })
 
-    console.log("file is uploaded successfully",response.url);
+    fs.unlinkSync(localFilePath);
 
     return response;
     
