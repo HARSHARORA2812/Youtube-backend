92e6e13081797643d00fce9055d3f27ffcbed6b8
Harsh Arora <officialharsharora2812@gmail.com>
2024-02-27 03:43:26 +0530

    New routers added

--- src/routs/user.routs.js
+++ src/routs/user.routs.js
@@ -1,7 +1,18 @@
 import { Router  } from "express";
 
 import {upload} from "../middleware/multer.middleware.js"
-import { logOutUser, loginUser, registerUser ,refreshAccessToken } from "../controllers/user.controllers.js";
+import { logOutUser,
+         loginUser, 
+         registerUser,
+         refreshAccessToken,
+         changeCurrentPassword,
+         getCurrentUser,
+         updateAcountDetails, 
+         updateUserAvatar, 
+         updateUserCoverImage, 
+         getUserChannelProfile, 
+         getWatchHistory } 
+   from "../controllers/user.controllers.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router = Router();
@@ -23,5 +34,19 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT , logOutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/hange-password").post( verifyJWT , changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT , getCurrentUser)
+
+router.route("upgrade-account-details").patch(verifyJWT , updateAcountDetails)
+
+router.route("/update-avatar").patch(verifyJWT , upload.single("avatar") , updateUserAvatar)
+
+router.route("/update-coverImage").patch(verifyJWT , upload.single("coverImage") , updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT , getUserChannelProfile)
+
+router.route("/watchHistory").get(verifyJWT , getWatchHistory)
 
 export default router
