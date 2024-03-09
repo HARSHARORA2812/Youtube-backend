3694de12710daf6f5dc90ff91789f65d3d865fe2
Harsh Arora <officialharsharora2812@gmail.com>
2024-03-09 15:09:46 +0530

    added route in apps

--- src/app.js
+++ src/app.js
@@ -19,7 +19,22 @@ app.use(cookieParser())
 // import Routers
 
 import userRouter from  "./routs/user.routs.js"
+import tweetRouter from  "./routs/tweet.routs.js"
+import playlistRouter from  "./routs/playlist.routs.js"
+import videoRouter from  "./routs/video.routs.js"
+import commentRouter from  "./routs/comment.routs.js"
+import likeRouter from  "./routs/like.routs.js"
+import subscriptionRouter from  "./routs/subscription.routs.js"
+import dashboardRouter from  "./routs/dashboard.routs.js"
 
 app.use("/api/v1/user",userRouter)
+app.use("/api/v1/tweet",tweetRouter)
+app.use("/api/v1/playlist",playlistRouter)
+app.use("/api/v1/video",videoRouter)
+app.use("/api/v1/comment",commentRouter)
+app.use("/api/v1/like",likeRouter)
+app.use("/api/v1/subscription",subscriptionRouter)
+app.use("/api/v1/dashboard",dashboardRouter)
+
 
 export { app }
